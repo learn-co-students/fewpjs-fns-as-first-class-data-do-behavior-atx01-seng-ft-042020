1ceaf37915a4e5e7fef78d2b9da4e4507f0afe78
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-06-01 21:45:18 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeString) {
+  const timeParsed = parseInt(timeString.split(':').join(''));
+  if (timeParsed < 1200) {
+    return "Good Morning";
+  } else if (timeParsed < 1700) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(message) {
+  document.querySelector('#greeting').innerText = message;
+}
